0ded798a41c7af56787be1c4487b5c66b113e497
khanburhan <k.khanburhan@gmail.com>
2022-06-16 14:18:44 +0300

    new coed added, still reqires some work, special characters not working

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,9 +1,10 @@
 
 import string
-user_string = input("Please Enter text to Cipher\n")
+user_string = input("Please Enter text to Cipher\n").lower()
 shift_value = input(
     "Please Input The Shifting Value. Use Can use Either a Positive or Negative Value. \n")
 
+print(user_string)
 shift_value = int(shift_value)
 # text_array = user_string.split(word)
 # print(text_array)
@@ -18,15 +19,25 @@ text_array = (split(user_string))
 
 
 univals = []
+new_univals = []
 for t in text_array:
     univals.append(ord(t)+shift_value)
     # print(univals)
+for x in univals:
+    if x == 32:
+        new_univals.append(x)
+    elif x > 122:
+        new_univals.append((x - 122) + 96)
+    elif x < 96:
+        new_univals.append(x + 122)
+    else:
+        new_univals.append(x)
+    # print(new_univals)
+""" for p in new_univals:
+    print(p)
+    print(chr(p)) """
 
-    # for i in univals:
-    #plus1_univals = univals + 1
-    # print(plus1_univals)
-
-    # print(type(plus1_univals))
-    cipher_text = ''.join(chr(i) for i in univals)
+cipher_text = ''.join(chr(i) for i in new_univals)
+# print(univals)
 
 print("The Ciphered Text Is: ", cipher_text)
